1ac253d28f26419b0efbf27548b1cb3ced638bf3
Yatin Sarbalia <yatinsns@gmail.com>
2016-03-04 18:34:14 +0530

    Create outgoing webhook callback URL support

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'json'
 
 module ChatDemo
   class App < Sinatra::Base
@@ -10,6 +11,18 @@ module ChatDemo
       content_type :js
       @scheme = ENV['RACK_ENV'] == "production" ? "wss://" : "ws://"
       erb :"application.js"
+    end
+
+    post "/support" do
+      uri = URI.parse(ENV["REDISCLOUD_URL"])
+      @redis ||= Redis.new(host: uri.host, port: uri.port, password: uri.password)
+      puts "#{params}"
+      data = JSON.parse(request.body.read.to_s)
+      puts data['text']
+      hash = Hash.new
+      hash['handle'] = "Support"
+      hash['text'] = data['text']
+      @redis.publish("chat-demo", hash.to_json)
     end
   end
 end
