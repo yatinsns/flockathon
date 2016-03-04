c81cb8153bf9dc147ca748ab55f5437c5e09bdcc
Yatin Sarbalia <yatinsns@gmail.com>
2016-03-05 01:09:23 +0530

    Send message before returning value

--- app.rb
+++ app.rb
@@ -26,6 +26,8 @@ module ChatDemo
       if params['incoming-url'].nil? || params['outgoing-token'].nil? || params['support-name'].nil? || params['welcome-message'].nil?
         { :error => "Keys missing"}.to_json
       else
+        configuration_message = "This group will now receive messages from users using Flockster."
+	`curl -X POST -d '{"text":"#{configuration_message}"}' -H "Content-Type:application/json;charset=UTF-8" https://api.flock.co/hooks/sendMessage/df4df2e4-c2fe-4f70-86fe-7bfdd09c7b15`
         outgoing_token = params['outgoing-token']
 	current_uuid = @redis.get(outgoing_token)
 	if current_uuid.nil?
@@ -36,8 +38,6 @@ module ChatDemo
 	else
           { :uuid => current_uuid}.to_json
 	end
-	configuration_message = "This group will now receive messages from users using Flockster."
-	`curl -X POST -d '{"text":"#{configuration_message}"}' -H "Content-Type:application/json;charset=UTF-8" https://api.flock.co/hooks/sendMessage/df4df2e4-c2fe-4f70-86fe-7bfdd09c7b15`
       end
     end
 
