37997dfd8bb9fe8ea575500e540346fe409dbbff
Yatin Sarbalia <yatinsns@gmail.com>
2016-03-05 01:44:21 +0530

    Fix company hash

--- app.rb
+++ app.rb
@@ -32,7 +32,12 @@ module ChatDemo
 	current_uuid = @redis.get(outgoing_token)
 	if current_uuid.nil?
           uuid = SecureRandom.uuid
-          @redis.set(uuid, params)
+	  hash = Hash.new
+	  hash['incoming-url'] = params['incoming-url']
+	  hash['outgoing-token'] = params['outgoing-token']
+	  hash['support-name'] = params['support-name']
+	  hash['welcome-message'] = params['welcome-message']
+          @redis.set(uuid, hash)
 	  @redis.set(outgoing_token, uuid)
           { :uuid => uuid }.to_json
 	else
@@ -55,6 +60,17 @@ module ChatDemo
         hash['text'] = text
 	hash['user'] = user
         @redis.publish("chat-demo", hash.to_json)
+      else
+	if user == "This group will now receive messages from users using Flockster."
+          puts "Received configuration message back"
+	  outgoing_token = params['token']
+	  uuid = @redis.get(outgoing_token)
+	  hash_string = @redis.get(uuid)
+	  puts "hash is #{hash_string}"
+	  hash = JSON.parse hash_string.gsub('=>', ':')
+	  hash['guid'] = data['from'].split('/')[0]
+          @redis.set(uuid, hash)
+	end
       end
     end
   end
